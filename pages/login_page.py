73d9911e71595977e6b9b0091d24e54b48cb0c18
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2019-10-21 13:03:25 +0500

    Initial commit

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        current_url = self.browser.current_url
+        current_url_list_without_empty_strings = [part for part in current_url.split('/') if part != '']
+
+        assert current_url_list_without_empty_strings[-1] == 'login', "Page URL don't ends with 'login'"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+            "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), \
+            "Registration form is not presented"
