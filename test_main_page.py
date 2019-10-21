ff2043b697591a3e8c5483a5e77fa63c5899f62f
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2019-10-21 13:07:51 +0500

    Add click to Login

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.should_be_login_link()      # проверяем наличие ссылки авторизации
+    main_page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    main_page.open()                      # открываем страницу
+    main_page.should_be_login_link()      # проверяем наличие ссылки авторизации
+    main_page.go_to_login_page()          # переходим на страницу авторизации
