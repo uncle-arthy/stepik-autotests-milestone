db94b5e2e2b561155ee99c4902bc380276b879d9
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2019-10-21 13:24:41 +0500

    Init and test login page

--- test_main_page.py
+++ test_main_page.py
@@ -8,3 +8,6 @@ def test_guest_can_go_to_login_page(browser):
     main_page.open()                      # открываем страницу
     main_page.should_be_login_link()      # проверяем наличие ссылки авторизации
     main_page.go_to_login_page()          # переходим на страницу авторизации
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()     # проверяем наличие элементов и адрес страницы авторизации
