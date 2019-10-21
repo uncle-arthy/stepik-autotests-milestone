32f32e1aa715e0ff26dae1c5b441723b547cf8e9
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2019-10-22 00:20:49 +0500

    Initial commit
    Implement add to cart click and checking of messageboxes

---
+++ pages/product_page.py
@@ -0,0 +1,36 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        add_to_cart_button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
+        add_to_cart_button.click()
+
+    def should_be_infoboxes(self):
+        self.should_be_product_added_to_cart_box()
+        self.should_be_same_title_in_success_box_and_in_description()
+        self.should_be_total_cart_price_box()
+        self.should_be_same_total_cart_price_and_product_price()
+
+    def should_be_product_added_to_cart_box(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_CART_SUCCESS_BOX), \
+            "No 'Successfully Add to cart' messagebox"
+
+    def should_be_same_title_in_success_box_and_in_description(self):
+        product_name_in_messagebox = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_SUCCESS_BOX).text
+        product_name_in_description = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_DESCRIPTION).text
+
+        assert product_name_in_description == product_name_in_messagebox, "Product names in messagebox and" \
+                                                                          "in description doesn't match"
+
+    def should_be_total_cart_price_box(self):
+        assert self.is_element_present(*ProductPageLocators.TOTAL_PRICE_IN_CART_BOX), \
+            "No 'Total price in a cart' messagebox"
+
+    def should_be_same_total_cart_price_and_product_price(self):
+        price_in_messageboox = self.browser.find_element(*ProductPageLocators.TOTAL_PRICE_IN_BOX).text
+        price_in_description = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_DESCRIPTION).text
+
+        assert price_in_description == price_in_messageboox, "Prices in messagebox and " \
+                                                             "in description doesn't match"
