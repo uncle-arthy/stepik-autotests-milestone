a6b6a8e7c218a49e5c1ab70bb1240544fe98cdd8
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2019-10-22 13:25:28 +0500

    Add check for login link and it works

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 
 urls = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -15,6 +16,7 @@ urls = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?pro
 
 
 @pytest.mark.parametrize('link', urls)
+@pytest.mark.skip(reason="Just for now")
 def test_guest_can_add_product_to_basket(browser, link):
     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -24,3 +26,52 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page.add_to_cart()
     product_page.solve_quiz_and_get_code()
     product_page.should_be_infoboxes()
+
+
+@pytest.mark.xfail()
+@pytest.mark.skip(reason="Just for now")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+
+    product_page.add_to_cart()
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.skip(reason="Just for now")
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.xfail()
+@pytest.mark.skip(reason="Just for now")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+
+    product_page.add_to_cart()
+    product_page.should_be_dissapearing_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_login_link()
+    product_page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
