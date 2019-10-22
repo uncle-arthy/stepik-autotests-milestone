cb227c8abf3d25f0a44a6b416472472cf0e4314b
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2019-10-22 22:26:21 +0500

    Add Test class w/ user registration setup fixture

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
@@ -91,3 +92,34 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
     basket_page.should_be_basket_is_empty_message()
+
+
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        registration_link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        login_page = LoginPage(browser, registration_link)
+        login_page.open()
+
+        fake_user_email = str(time.time()) + "@fakeusermail.org"
+        fake_user_password = "FaKeUsErPaSsWoRd"
+
+        login_page.register_new_user(fake_user_email, fake_user_password)
+        login_page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+
+        product_page.add_to_basket()
+        product_page.solve_quiz_and_get_code()
+        product_page.should_be_infoboxes()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+
+        product_page.should_not_be_success_message()
