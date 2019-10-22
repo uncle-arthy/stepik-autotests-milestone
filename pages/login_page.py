fe73a88cd27b1a030cf61ab3bffcf29b715e6d62
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2019-10-22 22:28:45 +0500

    Add user registration routine

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,24 +3,32 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.REGISTRATION_FORM_EMAIL_FIELD)
+        email_field.send_keys(email)
+        password_field = self.browser.find_element(*LoginPageLocators.REGISTRATION_FORM_PASSWORD_FIELD)
+        confirm_password_field = self.browser.find_element(*LoginPageLocators.REGISTRATION_FORM_CONFIRM_PASSWORD_FIELD)
+        password_field.send_keys(password)
+        confirm_password_field.send_keys(password)
+
+        register_btn = self.browser.find_element(*LoginPageLocators.REGISTRATION_FORM_REGISTER_BUTTON)
+        register_btn.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         current_url = self.browser.current_url
         current_url_list_without_empty_strings = [part for part in current_url.split('/') if part != '']
 
         assert current_url_list_without_empty_strings[-1] == 'login', "Page URL don't ends with 'login'"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
             "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), \
             "Registration form is not presented"
