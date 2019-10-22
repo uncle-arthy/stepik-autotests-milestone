6e5fe5905d6defb9175f04d651324e3b3f3e392e
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2019-10-22 15:51:53 +0500

    Wrap login tests in class

--- test_main_page.py
+++ test_main_page.py
@@ -5,17 +5,6 @@ from .pages.basket_page import BasketPage
 
 
 @pytest.mark.skip(reason="Just for now")
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.should_be_login_link()
-    main_page.go_to_login_page()
-
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     main_page = MainPage(browser, link)
@@ -27,3 +16,22 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
     basket_page.should_be_basket_is_empty_message()
+
+
+@pytest.mark.login_quest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.should_be_login_link()
+        main_page.go_to_login_page()
+
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.should_be_login_link()
